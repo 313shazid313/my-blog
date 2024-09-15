00eb9f318e58554a0c7e32382c82bae07fb7125c
313shazid313 <hossainshazid567@gmail.com>
2024-09-15 22:45:10 +0600

    project init

---
+++ cliant/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors:{
+        "electric-blue":"#7DF9FF"
+      }
+    },
+  },
+  plugins: [],
+};
