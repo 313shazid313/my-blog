00df69698c1408f3cce4205b0236b6adc976ea87
313shazid313 <hossainshazid567@gmail.com>
2024-09-18 23:19:33 +0600

    project init

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,25 @@
 const Navbar = () => {
   return (
-    <div className="flex justify-end pt-2 ">
-      <ul>
-        <li className="inline-block pr-5 ">topG</li>
-        <li className="inline-block pr-5">topG</li>
-        <li className="inline-block pr-5">topG</li>
-        <li className="inline-block pr-5">topG</li>
-      </ul>
-      <hr /> <hr /> <hr />
-    
+    <div>
+      <div className="flex justify-end p-2 ">
+        <ul>
+          <li className="inline-block pr-4 pl-5 ">topG</li>
+          <li className="inline-block pr-4 pl-5">topG</li>
+          <li className="inline-block pr-4 pl-5">topG</li>
+          <li className="inline-block pr-4 pl-5">topG</li>
+        </ul>
+      </div>
+      <hr className="border-gray" />
+
+      <div>
+        <ul className="flex justify-start p-2">
+          <li className="text-2xl ">myBlog</li>
+          <li className="inline-block pr-4 pl-5 ">topg</li>
+          <li className="inline-block pr-4 pl-5 ">topg</li>
+          <li className="inline-block pr-4 pl-5 ">topg</li>
+          <li className="inline-block pr-4 pl-5 ">topg</li>
+        </ul>
+      </div>
     </div>
   );
 };
